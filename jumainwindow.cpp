6602aae9339d80e622db0f54a7b4294acf77e991
JustusPan <jpanchn@gmail.com>
2016-10-29 22:05:42 +0800

    support zoom function

--- jumainwindow.cpp
+++ jumainwindow.cpp
@@ -17,6 +17,7 @@ JuMainWindow::JuMainWindow(QWidget *parent) :
     _model(0),
     _imageLabel(new QLabel),
     _scrollArea(new QScrollArea),
+    _scaleFactor(1),
     _openAct(0)
 {
     ui->setupUi(this);
@@ -46,6 +47,8 @@ bool JuMainWindow::loadFile(const QString &file) {
         qDebug()<<"error:empty book!";//debug
         return false;
     } else {
+        delete _model;
+        _model = NULL;
         setModel(newModel);
 
         const QImage newImage = _model->curPage();
@@ -55,6 +58,7 @@ bool JuMainWindow::loadFile(const QString &file) {
             return false;
         }
         setImage(newImage);
+        updateActions();
 
         this->setWindowFilePath(file);
 
@@ -70,7 +74,15 @@ void JuMainWindow::setImage(const QImage &newImage) {
     _image = newImage;
     _imageLabel->setPixmap(QPixmap::fromImage(_image));
     _scrollArea->setVisible(true);
-    _imageLabel->resize(_imageLabel->pixmap()->size());
+    scaleImage(1);
+    //_imageLabel->resize(_scaleFactor*_imageLabel->pixmap()->size());
+}
+
+void JuMainWindow::scaleImage(double factor) {
+    _scaleFactor *= factor;
+    _imageLabel->resize(_scaleFactor*_imageLabel->pixmap()->size());
+    _zoomInAct->setEnabled(_scaleFactor < 3);
+    _zoomOutAct->setEnabled(_scaleFactor > 0.5);
 }
 
 void JuMainWindow::open() {
@@ -82,11 +94,82 @@ void JuMainWindow::open() {
 
 }
 
+void JuMainWindow::nextPage() {
+    qDebug()<<"next page!";//debug
+    const QImage newImage = _model->nextPage();
+    if (newImage.isNull()) {
+        QMessageBox::information(this, QGuiApplication::applicationDisplayName(),
+                                tr("Cannot load"));
+        return;
+    }
+    setImage(newImage);
+}
+
+void JuMainWindow::prevPage() {
+    qDebug()<<"prev page!";//debug
+    const QImage newImage = _model->prevPage();
+    if (newImage.isNull()) {
+        QMessageBox::information(this, QGuiApplication::applicationDisplayName(),
+                                tr("Cannot load"));
+        return;
+    }
+    setImage(newImage);
+}
+
+void JuMainWindow::zoomIn() {
+    qDebug()<<"zoomIn";//debug
+    scaleImage(1.25);
+
+}
+
+void JuMainWindow::zoomOut() {
+    qDebug()<<"zoomOut";//debug
+    scaleImage(0.8);
+
+}
+
+void JuMainWindow::normalSize() {
+    qDebug()<<"normalSize";//debug
+    _scaleFactor=1;
+    scaleImage(1);
+}
+
 void JuMainWindow::createActions() {
+    //for file Menu
     QMenu *fileMenu = menuBar()->addMenu(tr("&File"));
 
     QAction *_openAct = fileMenu->addAction(tr("&Open..."), this, &JuMainWindow::open);
     _openAct->setShortcut(QKeySequence::Open);
+
+    //for go Menu
+    QMenu *goMenu = menuBar()->addMenu(tr("&Go"));
+    _nextPageAct = goMenu->addAction(tr("&NextPage"), this, &JuMainWindow::nextPage);
+    _nextPageAct->setShortcut(QKeySequence::MoveToNextChar);
+    _nextPageAct->setEnabled(false);
+    _prevPageAct = goMenu->addAction(tr("&PrevPage"), this, &JuMainWindow::prevPage);
+    _prevPageAct->setShortcut(QKeySequence::MoveToPreviousChar);
+    _prevPageAct->setEnabled(false);
+
+    //for zoom Menu
+    QMenu *zoomMenu = menuBar()->addMenu(tr("&Zoom"));
+    _zoomInAct = zoomMenu->addAction(tr("Zoom &In"), this, &JuMainWindow::zoomIn);
+    _zoomInAct->setShortcut(QKeySequence::ZoomIn);
+    _zoomInAct->setEnabled(false);
+    _zoomOutAct = zoomMenu->addAction(tr("Zoom &Out"), this, &JuMainWindow::zoomOut);
+    _zoomOutAct->setShortcut(QKeySequence::ZoomOut);
+    _zoomOutAct->setEnabled(false);
+    _normalSizeAct = zoomMenu->addAction(tr("&Normal Size"),this,&JuMainWindow::normalSize);
+    _normalSizeAct->setShortcut(QKeySequence("Ctrl+N"));
+    _normalSizeAct->setEnabled(false);
+
+}
+
+void JuMainWindow::updateActions() {
+   _nextPageAct->setEnabled(!_image.isNull());
+   _prevPageAct->setEnabled(!_image.isNull());
+   _zoomInAct->setEnabled(!_image.isNull());
+   _zoomOutAct->setEnabled(!_image.isNull());
+   _normalSizeAct->setEnabled(!_image.isNull());
 }
 
 static void initializeImageFileDialog(QFileDialog &dialog, QFileDialog::AcceptMode acceptMode)
--- jumainwindow.h
+++ jumainwindow.h
@@ -23,24 +23,36 @@ public:
 
 private slots:
     void open();
+    void nextPage();
+    void prevPage();
+    void zoomIn();
+    void zoomOut();
+    void normalSize();
 private:
     Ui::JuMainWindow *ui;
 
     void setModel(JuBookModel *model) { _model = model;}
     void createActions();
+    void updateActions();
     //void createMenus();
     void setImage(const QImage &newImage);
+    void scaleImage(double factor);
 
     JuBookModel *_model;
 
     QImage _image;
     QLabel *_imageLabel;
     QScrollArea *_scrollArea;
+    double _scaleFactor;
 
     QAction *_openAct;
 
     QAction *_nextPageAct;
     QAction *_prevPageAct;
+
+    QAction *_zoomInAct;
+    QAction *_zoomOutAct;
+    QAction *_normalSizeAct;
 
 };
 
